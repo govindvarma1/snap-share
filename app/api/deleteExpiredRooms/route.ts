5b346c4daa93653747e353cf69c7e6eb982f4360
Kallepalli Sai Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-12-20 13:04:34 +0530

    Add 15-min cron for deleting expired rooms

---
+++ app/api/deleteExpiredRooms/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+	try {
+		const now = new Date();
+		const threshold = new Date(now.getTime() - 15 * 60 * 1000);
+
+		const deletedRooms = await prisma.room.deleteMany({
+			where: {
+				createdAt: {
+					lt: threshold,
+				},
+			},
+		});
+		console.log(`Deleted ${deletedRooms.count} expired rooms`);
+		return NextResponse.json(
+			{ msg: `Deleted ${deletedRooms.count} expired rooms`, success: true },
+			{ status: 200 }
+		);
+	} catch (error) {
+		NextResponse.json(
+			{ error: (error as Error).message, success: false },
+			{ status: 500 }
+		);
+	}
+}
