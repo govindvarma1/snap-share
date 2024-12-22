e5d91b9d47796cd1328a22f8772778e7b2497728
Kallepalli Sai Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-12-22 22:07:47 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,32 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-    title: "SnapShare - Effortless File Sharing",
-    description: "Share files securely and quickly with SnapShare. Generate room codes and share files seamlessly without hassle.",
-    keywords: ["SnapShare", "file sharing", "secure file sharing", "quick sharing", "room codes", "file transfer"],
-    openGraph: {
-        title: "SnapShare - Secure File Sharing",
-        description: "SnapShare lets you share files securely with unique room codes. No account needed—just fast, seamless file transfers.",
-        url: "https://snapshareapp.vercel.app",
-        siteName: "SnapShare",
-        locale: "en_US",
-        type: "website",
-    },
+export const metadata: Metadata = {
+	title: "SnapShare - Effortless File Sharing",
+	description:
+		"Share files securely and quickly with SnapShare. Generate room codes and share files seamlessly without hassle.",
+	keywords: [
+		"SnapShare",
+		"file sharing",
+		"secure file sharing",
+		"quick sharing",
+		"room codes",
+		"file transfer",
+	],
+	openGraph: {
+		title: "SnapShare - Secure File Sharing",
+		description:
+			"SnapShare lets you share files securely with unique room codes. No account needed—just fast, seamless file transfers.",
+		url: "https://snapshareapp.vercel.app",
+		siteName: "SnapShare",
+		locale: "en_US",
+		type: "website",
+	},
 };
-
 
 export default function RootLayout({
 	children,
@@ -29,8 +38,9 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={`${inter.className} antialiased`}>
 				{children}
+				<Toaster position="bottom-left" />
+				<Analytics />
 			</body>
-			<Toaster position="bottom-left" />
 		</html>
 	);
 }
